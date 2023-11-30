5c4db58a0f4c420f5de1f42fa0db7a36ef3fc46d
Lucaaran <143127243+Lucaaran@users.noreply.github.com>
2023-11-30 09:44:57 -0500

    Pause

---
+++ Global.gd
@@ -0,0 +1,17 @@
+extends Node
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
+func _input(_event):
+	if Input.is_action_just_pressed("Menu"):
+		var menu = get_node_or_null("/root/Game/UI/Pause")
+		if menu == null:
+			get_tree().quit()
+		else:
+			if menu.visible:
+				get_tree().paused = false
+				menu.hide()
+			else:
+				get_tree().paused = true
+				menu.show()
